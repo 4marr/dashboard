bfa6e1b65d1ce7cc7dc5f2da8832a29fbae44ce2
Ammar <144656550+4marr@users.noreply.github.com>
2024-07-30 13:48:09 +0100

    Update script.js

--- DASHBOARD/script.js
+++ DASHBOARD/script.js
@@ -75,7 +75,7 @@ switchMode.addEventListener('change', function () {
 	}
 })
 
-const apiURL = `https://counterpro.vercel.app/api/count/?id=66`
+const apiURL = `https://counterpro.vercel.app/api/count/?id=aduyhaiuqwjyywgqbhqgwatgedajezxa`
 
 fetch(apiURL)
 .then((res) => {
