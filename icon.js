ca90a18dd4e6d4057040b6211a5ba06a0cf68cd6
Ammar <144656550+4marr@users.noreply.github.com>
2024-09-29 18:56:50 +0700

    Add files via upload

---
+++ icon.js
@@ -0,0 +1,508 @@
+export function CloudSunIcon() {
+    return `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12 2v2" />
+        <path d="m4.93 4.93 1.41 1.41" />
+        <path d="M20 12h2" />
+        <path d="m19.07 4.93-1.41 1.41" />
+        <path d="M15.947 12.65a4 4 0 0 0-5.925-4.128" />
+        <path d="M13 22H7a5 5 0 1 1 4.9-6H13a3 3 0 0 1 0 6Z" />
+      </svg>
+    `
+}
+export function CloudSunIconSmall() {
+    return `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12 2v2" />
+        <path d="m4.93 4.93 1.41 1.41" />
+        <path d="M20 12h2" />
+        <path d="m19.07 4.93-1.41 1.41" />
+        <path d="M15.947 12.65a4 4 0 0 0-5.925-4.128" />
+        <path d="M13 22H7a5 5 0 1 1 4.9-6H13a3 3 0 0 1 0 6Z" />
+      </svg>
+    `
+}
+
+export function SunIcon() {
+    return `
+      <svg
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <circle cx="12" cy="12" r="4" />
+   <path d="M12 2v2" />
+   <path d="M12 20v2" />
+   <path d="m4.93 4.93 1.41 1.41" />
+   <path d="m17.66 17.66 1.41 1.41" />
+   <path d="M2 12h2" />
+   <path d="M20 12h2" />
+   <path d="m6.34 17.66-1.41 1.41" />
+   <path d="m19.07 4.93-1.41 1.41" />
+      </svg>
+    `
+}
+export function SunIconSmall() {
+    return `
+      <svg
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <circle cx="12" cy="12" r="4" />
+   <path d="M12 2v2" />
+   <path d="M12 20v2" />
+   <path d="m4.93 4.93 1.41 1.41" />
+   <path d="m17.66 17.66 1.41 1.41" />
+   <path d="M2 12h2" />
+   <path d="M20 12h2" />
+   <path d="m6.34 17.66-1.41 1.41" />
+   <path d="m19.07 4.93-1.41 1.41" />
+      </svg>
+    `
+}
+
+export function SunMediumIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <circle cx="12" cy="12" r="4" />
+   <path d="M12 3v1" />
+   <path d="M12 20v1" />
+   <path d="M3 12h1" />
+   <path d="M20 12h1" />
+   <path d="m18.364 5.636-.707.707" />
+   <path d="m6.343 17.657-.707.707" />
+   <path d="m5.636 5.636.707.707" />
+   <path d="m17.657 17.657.707.707" />
+      </svg>
+    `
+}
+export function SunMediumIconSmall() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <circle cx="12" cy="12" r="4" />
+   <path d="M12 3v1" />
+   <path d="M12 20v1" />
+   <path d="M3 12h1" />
+   <path d="M20 12h1" />
+   <path d="m18.364 5.636-.707.707" />
+   <path d="m6.343 17.657-.707.707" />
+   <path d="m5.636 5.636.707.707" />
+   <path d="m17.657 17.657.707.707" />
+      </svg>
+    `
+
+
+}
+
+export function CloudIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
+      </svg>
+    `
+}
+export function CloudIconSmall() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
+      </svg>
+    `
+}
+
+export function CloudFogIcon() {
+    return `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+        <path d="M16 17H7" />
+        <path d="M17 21H9" />
+      </svg>
+    `
+}
+export function CloudFogIconSmall() {
+    return `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+        <path d="M16 17H7" />
+        <path d="M17 21H9" />
+      </svg>
+    `
+}
+
+export function CloudRainIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+   <path d="M16 14v6" />
+   <path d="M8 14v6" />
+   <path d="M12 16v6" />
+      </svg>
+    `
+}
+export function CloudRainIconSmall() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+   <path d="M16 14v6" />
+   <path d="M8 14v6" />
+   <path d="M12 16v6" />
+      </svg>
+    `
+}
+
+export function CloudDrizzleIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+   <path d="M8 19v1" />
+   <path d="M8 14v1" />
+   <path d="M16 19v1" />
+   <path d="M16 14v1" />
+   <path d="M12 21v1" />
+   <path d="M12 16v1" />
+      </svg>
+    `
+}
+export function CloudDrizzleIconSmall() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+   <path d="M8 19v1" />
+   <path d="M8 14v1" />
+   <path d="M16 19v1" />
+   <path d="M16 14v1" />
+   <path d="M12 21v1" />
+   <path d="M12 16v1" />
+      </svg>
+    `
+}
+
+export function CloudMoonIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M13 16a3 3 0 1 1 0 6H7a5 5 0 1 1 4.9-6Z" />
+   <path d="M10.1 9A6 6 0 0 1 16 4a4.24 4.24 0 0 0 6 6 6 6 0 0 1-3 5.197" />
+      </svg>
+    `
+}
+export function CloudMoonIconSmall() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M13 16a3 3 0 1 1 0 6H7a5 5 0 1 1 4.9-6Z" />
+   <path d="M10.1 9A6 6 0 0 1 16 4a4.24 4.24 0 0 0 6 6 6 6 0 0 1-3 5.197" />
+      </svg>
+    `
+}
+
+export function CloudMoonRainIcon() {
+    return `
+      <svg
+
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M10.083 9A6.002 6.002 0 0 1 16 4a4.243 4.243 0 0 0 6 6c0 2.22-1.206 4.16-3 5.197" />
+   <path d="M3 20a5 5 0 1 1 8.9-4H13a3 3 0 0 1 2 5.24" />
+   <path d="M11 20v2" />
+   <path d="M7 19v2" />
+      </svg>
+    `
+}
+export function CloudMoonRainIconSmall() {
+    return `
+      <svg
+   xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+   <path d="M10.083 9A6.002 6.002 0 0 1 16 4a4.243 4.243 0 0 0 6 6c0 2.22-1.206 4.16-3 5.197" />
+   <path d="M3 20a5 5 0 1 1 8.9-4H13a3 3 0 0 1 2 5.24" />
+   <path d="M11 20v2" />
+   <path d="M7 19v2" />
+      </svg>
+    `
+}
+
+export function HeavyCloudIcon() {
+    return `
+      <svg
+   xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+        <path d="M17.5 21H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
+        <path d="M22 10a3 3 0 0 0-3-3h-2.207a5.502 5.502 0 0 0-10.702.5" />
+      </svg>`
+}
+export function HeavyCloudIconSmall() {
+    return `
+      <svg
+   xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+   viewBox="0 0 24 24"
+   fill="none"
+   stroke="currentColor"
+   strokeWidth="2"
+   strokeLinecap="round"
+   strokeLinejoin="round"
+      >
+        <path d="M17.5 21H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
+        <path d="M22 10a3 3 0 0 0-3-3h-2.207a5.502 5.502 0 0 0-10.702.5" />
+      </svg>`
+}
+
+export function WindIcon() {
+    return `  <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M17.7 7.7a2.5 2.5 0 1 1 1.8 4.3H2" />
+        <path d="M9.6 4.6A2 2 0 1 1 11 8H2" />
+        <path d="M12.6 19.4A2 2 0 1 0 14 16H2" />
+      </svg>
+    `
+}
+export function WindIconSmall() {
+    return `  <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M17.7 7.7a2.5 2.5 0 1 1 1.8 4.3H2" />
+        <path d="M9.6 4.6A2 2 0 1 1 11 8H2" />
+        <path d="M12.6 19.4A2 2 0 1 0 14 16H2" />
+      </svg>
+    `
+}
+
+export function FogIcon() {
+    return `  <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+      <path d="M20 10H5" />
+      <path d="M25 14H8" />
+      <path d="M20 18H5" />
+      </svg>
+    `
+}
+export function FogIconSmall() {
+    return `  <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+      <path d="M20 10H5" />
+      <path d="M25 14H8" />
+      <path d="M20 18H5" />
+     </svg>
+    `
+}
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+import * as icon from './icon.js';
+
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+allSideMenu.forEach(item => {
+    const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
+    item.addEventListener('click', function () {
+        allSideMenu.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
 });
 
 
@@ -19,7 +21,7 @@ const menuBar = document.querySelector('#content nav .bx-menu-alt-left');
 const sidebar = document.getElementById('sidebar');
 
 menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
+    sidebar.classList.toggle('hide');
 })
 
 
@@ -33,34 +35,34 @@ const searchButtonIcon = document.querySelector('#content nav form .form-input b
 const searchForm = document.querySelector('#content nav form');
 
 searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
+    if (window.innerWidth < 576) {
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchButtonIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
 })
 
 
 
 
 
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
+if (window.innerWidth < 768) {
+    sidebar.classList.add('hide');
+} else if (window.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
 }
 
 
 window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
+    if (this.innerWidth > 576) {
+        searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
 })
 
 
@@ -68,31 +70,80 @@ window.addEventListener('resize', function () {
 const switchMode = document.getElementById('switch-mode');
 
 switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-	}
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
 })
 
-const apiURL = `https://counterpro.vercel.app/api/count/?id=66`
+// Cek apakah ada data kunjungan sebelumnya
+let count = localStorage.getItem('count');
+if (count) {
+    document.getElementById('countVisitors').textContent = count;
+} else {
+    count = 0;
+}
 
-fetch(apiURL)
-.then((res) => {
-	return res.json();
-})
-.then((data) => {
-	data = data.count
-	console.log(data)
-	document.getElementById("countVisitors").innerHTML = data
-})
+// Tingkatkan nilai counter setiap kali halaman dimuat
+count++;
+localStorage.setItem('count', count);
 
+// clock
 function Time() {
-	let jam = new Date().getHours();
-	let menit = new Date().getMinutes();
-	let detik = new Date().getSeconds();
-	let gabungan = `${jam}:${menit}:${detik}`
-	document.getElementById("time").innerHTML = gabungan;
+    let hour = new Date().getHours();
+    let minute = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    let gabungan = `${hour}:${minute}:${seconds}`
+    document.getElementById("time").innerHTML = gabungan;
 }
 
 setInterval(Time, 1)
+
+// wheather
+let apiCuaca = "https://api.mininxd.my.id/bmkg/weather/jawaBarat/kotaBogor/bogorSelatan";
+fetch(apiCuaca)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data)
+        let temperature = data.weatherNow.temperature;
+        let weather = data.weatherNow.weather;
+
+        let containerTemp = document.getElementById("temperature");
+        let containerWeat = document.getElementById("weather");
+
+        containerTemp.innerHTML = temperature;
+        containerWeat.innerHTML = weather;
+
+        let weatherIcon = document.getElementById("weatherIcon");
+
+        if (data.weatherNow.weather.includes('Cerah Berawan')) {
+            weatherIcon.innerHTML = icon.CloudSunIcon();
+        }
+        else if (data.weatherNow.weather.includes('Cerah')) {
+            weatherIcon.innerHTML = icon.SunIcon();
+        }
+        else if (data.weatherNow.weather.includes('Berawan Tebal')) {
+            weatherIcon.innerHTML = icon.HeavyCloudIcon();
+        }
+        else if (data.weatherNow.weather.includes('Berawan')) {
+            weatherIcon.innerHTML = icon.CloudIcon();
+        }
+        else if (data.weatherNow.weather.includes('Hujan')) {
+            weatherIcon.innerHTML = icon.CloudRainIcon();
+        }
+        else if (data.weatherNow.weather.includes('Gerimis')) {
+            weatherIcon.innerHTML = icon.CloudDrizzleIcon();
+        }
+        else if (data.weatherNow.weather.includes('Udara Kabur')) {
+            weatherIcon.innerHTML = icon.CloudFogIcon();
+        }
+        else if (data.weatherNow.weather.includes('Berangin')) {
+            weatherIcon.innerHTML = icon.WindIcon();
+        }
+        else if (data.weatherNow.weather.includes('Kabut')) {
+            weatherIcon.innerHTML = icon.FogIcon();
+        }
+    })
